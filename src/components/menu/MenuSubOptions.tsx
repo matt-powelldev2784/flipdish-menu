ee6cd50b976a4e13184b3a71e2cd18b364964d14
Matthew Powell <matt.powell2784@gmail.com>
2024-11-12 21:13:22 +0000

    feat: filter menu items when master item is selected

--- src/components/menu/MenuSubOptions.tsx
+++ src/components/menu/MenuSubOptions.tsx
@@ -1,12 +1,16 @@
+/* eslint-disable prettier/prettier */
 import { useMenuContext } from 'cartContext/CartContext'
 import { MenuItemT } from 'menuData/menuData'
+import { useState } from 'react'
 
 interface MasterItemProps {
   menuItem: MenuItemT
 }
 
 export const MenuSubOptions = ({ menuItem }: MasterItemProps) => {
-  const { setCurrentMenuItemId, setCurrentMasterItemId } = useMenuContext()
+  const [masterItemSelected, setMasterItemSelected] = useState(false)
+  const { setCurrentMenuItemId, currentMasterItemId, setCurrentMasterItemId } =
+    useMenuContext()
   const menuOptions = menuItem.MenuItemOptionSets
 
   return (
@@ -19,37 +23,36 @@ export const MenuSubOptions = ({ menuItem }: MasterItemProps) => {
       </button>
       <p>{menuItem.Name}</p>
 
-      {menuOptions.map((menuOptions) => {
-        const isMasterOption = menuOptions.IsMasterOptionSet
-        const menuSubOptions = menuOptions.MenuItemOptionSetItems
+      {menuOptions.map((menuOption) => {
+        const isMasterOption = menuOption.IsMasterOptionSet
+        const menuItems = menuOption.MenuItemOptionSetItems.filter((item) => {
+          if (!masterItemSelected) return true
+          if (!isMasterOption) return true
 
-        return (
-          <div key={menuOptions.MenuItemOptionSetId}>
-            {menuSubOptions.map((menuSubOptions) => {
-              const subOptionId = menuSubOptions.MenuItemOptionSetItemId
-              return (
-                <div key={menuSubOptions.MenuItemOptionSetItemId}>
-                  <p>
-                    isMasterOption = {isMasterOption.toString()} -{' '}
-                    {menuSubOptions.Name} - {menuSubOptions.Price}
-                  </p>
-                  <button
-                    onClick={() => setCurrentMasterItemId(subOptionId)}
-                    className="bg-blue-500 p-2 text-xl"
-                  >
-                    Add
-                  </button>
-                  <button
-                    onClick={() => setCurrentMasterItemId(null)}
-                    className="bg-blue-500 p-2 text-xl"
-                  >
-                    Remove
-                  </button>
-                </div>
-              )
-            })}
-          </div>
-        )
+          return item.MenuItemOptionSetItemId === currentMasterItemId
+        })
+
+        return menuItems.map((menuItem) => {
+          return (
+            <div
+              key={menuItem.MenuItemOptionSetItemId}
+              className="m-4 flex flex-row justify-between"
+            >
+              <p>
+                {menuItem.Name}- {menuItem.Price}
+              </p>
+              <button
+                className="bg-slate-500 p-2"
+                onClick={() => {
+                  setCurrentMasterItemId(menuItem.MenuItemOptionSetItemId)
+                  setMasterItemSelected(true)
+                }}
+              >
+                Add Item
+              </button>
+            </div>
+          )
+        })
       })}
     </div>
   )
