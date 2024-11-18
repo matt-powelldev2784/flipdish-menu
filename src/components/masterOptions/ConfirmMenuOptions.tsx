42732d62ed40ee917ac559281fccd74d03384c57
Matthew Powell <matt.powell2784@gmail.com>
2024-11-18 20:59:53 +0000

    feat: add temp cart item confirmation list

--- src/components/masterOptions/ConfirmMenuOptions.tsx
+++ src/components/masterOptions/ConfirmMenuOptions.tsx
@@ -2,7 +2,19 @@ import { useMenuContext } from 'cartContext/MenuContext'
 
 export const ConfirmMenuOptions = () => {
   const { tempCartItem } = useMenuContext()
+  if (!tempCartItem) return <p>Server error</p>
   console.log('tempCartItem', tempCartItem)
 
-  return <div>test</div>
+  return (
+    <div>
+      <p>{tempCartItem.name}</p>
+      {tempCartItem.subOptions?.map((subOption) => {
+        return (
+          <p key={subOption.id}>
+            {subOption.name} - {subOption.price}
+          </p>
+        )
+      })}
+    </div>
+  )
 }
