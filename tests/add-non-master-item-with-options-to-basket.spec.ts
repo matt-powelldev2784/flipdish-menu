9232f5408fdc48bd2fc02aaaba1aa0be04a67e1a
Matthew Powell <matt.powell2784@gmail.com>
2024-11-23 20:15:19 +0000

    test: update test comments and description

--- tests/add-non-master-item-with-options-to-basket.spec.ts
+++ tests/add-non-master-item-with-options-to-basket.spec.ts
@@ -1,5 +1,5 @@
 import { expect, test } from '@playwright/test'
-test('user can add a menu item which is master item that has options to the basket', async ({
+test('user can add a menu item which is NOT a master option but has sub options to the basket', async ({
   page
 }) => {
   // navigate to home page
@@ -7,22 +7,22 @@ test('user can add a menu item which is master item that has options to the bask
 
   // Find the articles that have menu options
   // and filter the articles that do NOT contain the text "Select item for price options"
-  // This will return all articles that do NOT have master options and have menu options
-  const masterItemsArticles = page
+  // This will return all articles that are NOT have master options but have sub options
+  const menuItemArticles = page
     .locator('article[data-test-id="menuItemWithOptions"]')
     .filter({
       hasNotText: 'Select item for price options'
     })
 
-  // Press the first select button which relates to a menu item with master options
-  const selectItemButton = masterItemsArticles
+  // Press the menu item article select button
+  const selectItemButton = menuItemArticles
     .getByRole('button', { name: 'Select' })
     .first()
   await selectItemButton.click()
 
   // There could be multiple options pages
   // This part of the test allows for upto 8 options pages
-  //I have created an array with 8 slots to loop through
+  // I have created an array with 8 slots to loop through
   const maxExpectedOptions = [...Array(8).keys()]
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
