0b214d711b615eeb6b3a638ba47e2eae49970e52
Matthew Powell <matt.powell2784@gmail.com>
2024-11-24 12:45:46 +0000

    test: rename app test file and set test config filename matcher

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -2,10 +2,8 @@ import { screen } from '@testing-library/react'
 import { renderWithContext } from '../../tests/utils/renderWithContext'
 import { App } from './App'
 
-describe('<App />', () => {
-  it('should render the App', () => {
-    renderWithContext({ children: <App /> })
+test('should render the App', () => {
+  renderWithContext({ children: <App /> })
 
-    expect(screen.getByText('Menu')).toBeInTheDocument()
-  })
+  expect(screen.getByText('Menu')).toBeInTheDocument()
 })
