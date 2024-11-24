fc602c42039320854270afbe281bebad216db44c
Matthew Powell <matt.powell2784@gmail.com>
2024-11-24 13:19:29 +0000

    test: add test to check cart items total is correctly calculated

---
+++ src/components/cart/Cart.test.tsx
@@ -0,0 +1,30 @@
+import { screen } from '@testing-library/react'
+import { renderWithContext } from '../../../tests/utils/renderWithContext'
+import { Cart } from './Cart'
+import { cartItemsMock } from '../../../tests/utils/cartItemsMock'
+import { CartItem } from 'menuContext/MenuContext'
+
+const calculateCartTotalPrice = (cartItems: CartItem[]) => {
+  return cartItems.reduce((acc, item) => {
+    const subTotal = item.menuOptions?.reduce(
+      (acc, option) => acc + option.price * option.quantity,
+      0
+    )
+    return acc + item.price * item.quantity + (subTotal || 0)
+  }, 0)
+}
+
+const cartTotalPrice = calculateCartTotalPrice(cartItemsMock)
+
+test('should check cart items total price is correctly calculated', () => {
+  renderWithContext({
+    children: <Cart />,
+    contextValues: {
+      cartItems: cartItemsMock,
+      cartTotalPrice: cartTotalPrice
+    }
+  })
+
+  const totalPrice = screen.getByText('Total Price: £18.20')
+  expect(totalPrice).toBeInTheDocument()
+})
